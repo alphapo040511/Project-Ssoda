77ad2ef8e52f1bf3f7817046484b311156b7c3ca
namsuhan0322 <rnatpakfl@gmail.com>
2025-01-23 23:44:17 +0900

    [Fix] AttackStateData 0으로 초기화

--- Assets/Scripts/StatusData/AttackStateData.cs
+++ Assets/Scripts/StatusData/AttackStateData.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AttackStatusData", menuName = "Scriptable Object/AttackStatusData", order = int.MaxValue)]
 public class AttackStateData : ScriptableObject
 {
-    public float atkCooldown = 0.5f;            // 공격 속도 (초당)
-    public float atkRange = 10f;                // 사거리
-    public float projectileThickness = 0.8f;    // 발사체 두께
-    public float lastAttackTime = 0f;
-    public float projectileSpeed = 10f;         // 발사체 속도
-    public float projectileLifetime = 5f;       // 발사체 생존 시간
+    public float atkCooldown;               // 공격 속도 (초당)
+    public float atkRange;                  // 사거리
+    public float projectileThickness;       // 발사체 두께
+    public float lastAttackTime;
+    public float projectileSpeed;           // 발사체 속도
+    public float projectileLifetime;        // 발사체 생존 시간
 }
