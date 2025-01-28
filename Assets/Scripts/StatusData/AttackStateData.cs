310be13e93afaa54098274cf03b713282fea287e
namsuhan0322 <rnatpakfl@gmail.com>
2025-01-28 18:06:54 +0900

    [Fix] 중복코드 수정

--- Assets/Scripts/StatusData/AttackStateData.cs
+++ Assets/Scripts/StatusData/AttackStateData.cs
@@ -19,7 +19,6 @@ public class AttackStateData : ScriptableObject
     public float atkCooldown;               // 공격 속도 (초당)
     public float atkRange;                  // 사거리
     public float projectileThickness;       // 발사체 두께
-    public float lastAttackTime;
     public float projectileSpeed;           // 발사체 속도
     public float projectileLifetime;        // 발사체 생존 시간
 }
