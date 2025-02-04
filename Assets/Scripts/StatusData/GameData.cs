d384075284439c03b0bd3e29d4341ee8c3afc052
ssoyeeon <ssuu2039@gmail.com>
2025-02-04 15:55:53 +0900

    [feat] 데이터 저장 , 아이템 저장
    데이터는 계속 추가할거구요... 아이템은 스크래처블로 해서 ID 받아와서 하려했는데 맞는지 잘 모르겠어서 놔뒀습니다.

---
+++ Assets/Scripts/StatusData/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+    //인벤토리 , 해금 된 스테이지 , 업적 , 
+    public int nowStage;          //현재 스테이지
+    public int achievement;       //업적
+    public int[] inventoryID;     //인벤토리 ID 저장
+
+}
---
+++ Assets/Scripts/StatusData/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : ScriptableObject
+{
+    public int itemID;                  //아이템 아이디 
+    public string itemName;             //아이템 이름 
+    public string itemDescription;      //아이템 설명
+}
