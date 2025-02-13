91185b793abbaa87ae8342fdc4bc2c1a737847bf
ssoyeeon <ssuu2039@gmail.com>
2025-02-13 15:43:49 +0900

    [feat] ItemData

--- Assets/Scripts/Manager/EventManager.cs
+++ Assets/Scripts/Manager/EventManager.cs
@@ -7,16 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class EventManager : MonoBehaviour
 {
-    public event Action<bool> OnSceneChanger;       //씬 넘길 때 사용할 이벤트
-    public event Action<bool> OnNextStage;       //다음 스테이지로 넘어갈 때 사용할 이벤트
+    public event Action<int> Item;       //아이템 획득 시 발생할 이벤트
 
-    public void SceneChange()       //게임 시작, 종료, 상점, 설정창 등 들어갈 때
+    public void ItemInput()
     {
         
-    }
-
-    public void NextStage()         //다음 스테이지로 넘어갈 때 ( 몬스터 생성 및 플레이어 이동 )
-    {
-
     }
 }
--- Assets/Scripts/StatusData/ItemData.cs
+++ Assets/Scripts/StatusData/ItemData.cs
@@ -7,4 +7,5 @@ public class ItemData : ScriptableObject
     public int itemID;                  //아이템 아이디 
     public string itemName;             //아이템 이름 
     public string itemDescription;      //아이템 설명
+    public Sprite Itemspr;              //아이템 아이콘
 }
